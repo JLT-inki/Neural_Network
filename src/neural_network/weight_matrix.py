730d770b75d5eade24e325a2b8525da3fee124bc
Jan Krueger <jan-luca.krueger@web.de>
2023-11-29 10:17:26 +0100

    Added method to create WeightMAtrix objects from a CSV file. Also, added an inlcusion of a table header to the write_weights method.

--- src/neural_network/weight_matrix.py
+++ src/neural_network/weight_matrix.py
@@ -1,9 +1,9 @@
 """File containing the weight class."""
 
-# Imports necessary for the write_weights method
 from __future__ import annotations
 from pathlib import Path
 
+import ast
 import csv
 import _csv
 
@@ -22,6 +22,8 @@ class WeightMatrix:
         Return the weight matrix.
     write_weights
         Write weight matrices into a CSV file.
+    create_weights_from_csv
+        Read a CSV file and create a weight matrix per line that is read.
 
     """
 
@@ -72,6 +74,9 @@ class WeightMatrix:
         # Check if the file exists
         file_exists: bool = Path(path_to_output).is_file()
 
+        # Define the table header
+        header: list[str] = ["weights"]
+
         with open(path_to_output, 'w', encoding='utf-8') as csv_file:
             # Initialize the writer of the CSV file
             csv_writer: _csv._writer = csv.writer(csv_file)
@@ -80,6 +85,47 @@ class WeightMatrix:
             if file_exists:
                 csv_file.truncate(0)
 
+            # Write the header
+            csv_writer.writerow(header)
+
             # Write each weight matrix
             for matrix in weight_matrices:
-                csv_writer.writerow(matrix.get_weights())
+                csv_writer.writerow([matrix.get_weights()])
+
+    @staticmethod
+    def create_weights_from_csv(path_to_csv_file: str) -> list[WeightMatrix]:
+        """
+        Read a CSV file and create a weight matrix per line that is read.
+
+        Parameters
+        ----------
+        path_to_csv_file: str
+            Path to the CSV file that is read.
+
+        Returns
+        -------
+        weight_matrix_list: list[WeightMatrix]
+            List containing all the weight matrices created.
+
+        Notes
+        -----
+        Import in line 4 is necessary for type hints of this function as the type
+        'WeightMatrix' is used as a forward reference here (see index 563 of the
+        Python Enhancement Proposals [PEP 563]).
+
+        """
+        # Initialize the return value
+        weight_matrix_list: list[WeightMatrix] = []
+
+        with open(path_to_csv_file, 'r', encoding='utf-8') as csv_file:
+            # Initialize the reader of the CSV file
+            csv_reader: csv.DictReader = csv.DictReader(csv_file)
+
+            # Read each row
+            for row in csv_reader:
+                # Read the weight matrix & create a WeightMatrix object
+                weight_matrix_list.append(WeightMatrix(
+                    ast.literal_eval(row["weights"])))
+
+        # Return the newly created WeightMatrix objects
+        return weight_matrix_list
